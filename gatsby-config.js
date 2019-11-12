efd0f37b687c6a626b9a50c67fd40d6f5487b6c4
Jonathan Navarrete <jonatns@gmail.com>
2019-11-11 21:08:33 -0800

    Added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-44793393-2',
       },
     },
     `gatsby-plugin-feed`,
